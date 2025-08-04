dc99338ede4a1627c0098a6b21db36dce88645dc
a123 <samindaniil97@gmail.com>
2025-08-04 16:34:33 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,29 +1,23 @@
-from playwright.sync_api import Page,expect
+from playwright.sync_api import sync_playwright, expect
 
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
 
-def test(page: Page):
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    email = page.locator('//input[@id=":r0:"]')
-    expect(email).to_be_visible()
-    email.fill("user.name@gmail.com")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-    username = page.locator('//input[@id=":r1:"]')
-    expect(username).to_be_visible()
-    username.fill("username")
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    password = page.locator('//input[@id=":r2:"]')
-    expect(password).to_be_visible()
-    password.fill("password")
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
-    button_element = page.locator('//button[@id="registration-page-registration-button"]')
-    expect(button_element).to_be_visible()
-    assert button_element.is_enabled(), "Кнопка должна быть доступна для клика"
-    button_element.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    header_exists = page.locator('h6[data-testid="dashboard-toolbar-title-text"]').is_visible()
-
-    if header_exists:
-        print("Заголовок 'Dashboard' присутствует на странице.")
-    else:
-        print("Заголовок 'Dashboard' отсутствует на странице.")
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text('Dashboard')
