2d1db5c6d8a84ca177d04c10b8bc55bb4b0f6a69
a123 <samindaniil97@gmail.com>
2025-08-04 16:35:22 +0300

    Углубление в Playwright. Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -1,23 +1,21 @@
-from playwright.sync_api import Page,expect
+from playwright.sync_api import sync_playwright, expect
 
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
 
-def test(page: Page):
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    button_element = page.locator('//button[@id="registration-page-registration-button"]')
-    expect(button_element).to_be_disabled()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_disabled()
 
-    email = page.locator('//input[@id=":r0:"]')
-    expect(email).to_be_visible()
-    email.fill("user.name@gmail.com")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
 
-    username = page.locator('//input[@id=":r1:"]')
-    expect(username).to_be_visible()
-    username.fill("username")
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
 
-    password = page.locator('//input[@id=":r2:"]')
-    expect(password).to_be_visible()
-    password.fill("password")
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
 
-    button_element2 = page.locator('//button[@id="registration-page-registration-button"]')
-    expect(button_element2).to_be_visible()
+    expect(registration_button).to_be_enabled()
