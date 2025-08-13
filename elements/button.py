eb8f983a39f862d30cabe73633f295d50b750922
a123 <samindaniil97@gmail.com>
2025-08-13 17:15:09 +0300

    Практикуемся в интеграции покрытия в UI тесты.Практическое задание: Работа с инструментом ui-coverage-tool3

--- elements/button.py
+++ elements/button.py
@@ -10,29 +10,25 @@ logger = get_logger("BUTTON")
 
 class Button(BaseElement):
     @property
-    def type_of(self) -> str:  # Переопределяем свойство type_of
+    def type_of(self) -> str:
         return "button"
 
     def check_enabled(self, nth: int = 0, **kwargs):
-        """Проверяет, что кнопка доступна для взаимодействия."""
         step = f'Checking that {self.type_of} "{self.name}" is enabled'
-        
+
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_enabled()
 
-        # Трекаем проверку доступности как ENABLED
         self.track_coverage(ActionType.ENABLED, nth, **kwargs)
 
     def check_disabled(self, nth: int = 0, **kwargs):
-        """Проверяет, что кнопка недоступна для взаимодействия."""
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
-        
+
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
 
-        # Трекаем проверку недоступности как DISABLED
         self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -10,40 +10,31 @@ logger = get_logger("TEXTAREA")
 
 class Textarea(BaseElement):
     @property
-    def type_of(self) -> str:  # Переопределяем свойство type_of
+    def type_of(self) -> str:
         return "textarea"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
-        """Переопределяем для работы с textarea тегами внутри элемента."""
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
     def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
-        """Переопределяем метод формирования XPath-селектора:
-         - сначала получаем общий селектор блока
-         - затем уточняем путь до самого <textarea>, добавляя '//textarea'
-        Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие."""
-        return f'{super().get_raw_locator(**kwargs)}//textarea'
+        return f'{super().get_raw_locator(nth, **kwargs)}//textarea[1]'
 
     def fill(self, value: str, nth: int = 0, **kwargs):
-        """Заполняет поле указанным текстом."""
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
-        
+
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
 
-        # После успешного fill фиксируем покрытие как действие FILL
         self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
-        """Проверяет значение, находящееся в поле ввода."""
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
-        
+
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
 
-        # Фиксируем в покрытии, что значение проверено — тип VALUE
         self.track_coverage(ActionType.VALUE, nth, **kwargs)
